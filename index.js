119b0dcbf637be42b470070117dfbf3e3d56984a
Elvis Onobo <elvis.onobo@gmail.com>
2025-01-21 07:50:20 +0100

    fix: verify webhook signature

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const crypto = require("crypto");
 
 const app = express();
 app.use(express.json());
@@ -7,11 +8,34 @@ app.get('/', (req, res) => {
     res.send('Hello World');
 })
 
-app.post('/test', (req, res) => {
+app.post('/test', verifyWebhookSignature, (req, res) => {
     res.json({
         test: "Hello World!"
     })
 })
+
+export const verifyWebhookSignature = async (req, res, next) => {
+    const payload = req.body
+    console.log(payload)
+    const githubSignature = req.headers['X-Hub-Signature-256'];
+    console.log({ githubSignature })
+    if (!githubSignature) {
+        res.status(401).send('No signature found');
+        return;
+    }
+    const hmac = crypto.createHmac('sha256', config.githubWebhookSecret);
+    hmac.update(payload, 'utf8');
+    const digest = `sha256=${hmac.digest('hex')}`;
+    console.log({ digest })
+    const isVerified = crypto.timingSafeEqual(Buffer.from(githubSignature), Buffer.from(digest));
+    console.log({ isVerified })
+    if (!isVerified) {
+        res.status(401).send('Signature verification failed');
+        return;
+    }
+    return next()
+}
+
 
 app.listen(3000, () => {
     console.log(`Server is running on port 3000`);
