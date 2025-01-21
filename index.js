82319a3530f31a395f1c102ab710e131a775a195
Elvis Onobo <elvis.onobo@gmail.com>
2025-01-21 07:43:21 +0100

    chore: add post test endpoint

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ app.get('/', (req, res) => {
     res.send('Hello World');
 })
 
+app.post('/test', (req, res) => {
+    res.json({
+        test: "Hello World!"
+    })
+})
+
 app.listen(3000, () => {
     console.log(`Server is running on port 3000`);
 })
