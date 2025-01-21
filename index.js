dee77b6011a25b308de24e0d47c0db721a7014cf
Elvis Onobo <elvis.onobo@gmail.com>
2025-01-21 08:05:35 +0100

    chore: update signature

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ app.post('/test', verifyWebhookSignature, (req, res) => {
 
 export const verifyWebhookSignature = async (req, res, next) => {
     const payload = req.body
-    console.log(payload)
-    const githubSignature = req.headers['X-Hub-Signature-256'];
+    console.log("Payload >>>>> ", payload)
+    const githubSignature = req.headers['x-hub-signature-256'];
     console.log({ githubSignature })
     if (!githubSignature) {
         res.status(401).send('No signature found');
@@ -26,7 +26,7 @@ export const verifyWebhookSignature = async (req, res, next) => {
     const hmac = crypto.createHmac('sha256', config.githubWebhookSecret);
     hmac.update(payload, 'utf8');
     const digest = `sha256=${hmac.digest('hex')}`;
-    console.log({ digest })
+    console.log("Digest >>>>> ", { digest })
     const isVerified = crypto.timingSafeEqual(Buffer.from(githubSignature), Buffer.from(digest));
     console.log({ isVerified })
     if (!isVerified) {
